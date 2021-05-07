00ac0840f3e930c07498e304b4d01551b7b73009
Robert Hammann <robert.hammann@outlook.com>
2021-05-07 13:13:57 +0200

    minimal distance for weighting function

--- GOFevaluation/evaluators_nd.py
+++ GOFevaluation/evaluators_nd.py
@@ -9,7 +9,7 @@ from GOFevaluation import test_statistics_sample
 
 class nd_test_statistics(test_statistics):
     """
-        Override binning to work in arbitrary dimensions
+    Override binning to work in arbitrary dimensions
     """
 
     def bin_data(self, bin_edges):
@@ -19,18 +19,18 @@ class nd_test_statistics(test_statistics):
 
 class binned_poisson_chi2_gof(nd_test_statistics):
     """
-        computes the binned poisson modified Chi2 from Baker+Cousins
-        In the limit of large bin counts (10+) this is Chi2 distributed.
-        In the general case you may have to toyMC the distribution yourself.
-        Input:
-         - nevents_expected: array with expected # of events (not rates) for each bin
-         - data: array of equal shape as nevents_expected
-             containing observed events in each bin
-        Output:
-         - the sum of the binned poisson Chi2.
-        reference: https://doi.org/10.1016/0167-5087(84)90016-4
-        While the absolute likelihood is a poor GOF measure
-        (see http://www.physics.ucla.edu/~cousins/stats/cousins_saturated.pdf)
+    computes the binned poisson modified Chi2 from Baker+Cousins
+    In the limit of large bin counts (10+) this is Chi2 distributed.
+    In the general case you may have to toyMC the distribution yourself.
+    Input:
+     - nevents_expected: array with expected # of events (not rates) for each bin
+     - data: array of equal shape as nevents_expected
+         containing observed events in each bin
+    Output:
+     - the sum of the binned poisson Chi2.
+    reference: https://doi.org/10.1016/0167-5087(84)90016-4
+    While the absolute likelihood is a poor GOF measure
+    (see http://www.physics.ucla.edu/~cousins/stats/cousins_saturated.pdf)
     """
 
     def __minimalinit__(self, data, expectations):
@@ -66,28 +66,31 @@ class binned_chi2_gof(nd_test_statistics):
 
 
 class point_to_point_gof(test_statistics_sample):
-    """computes point-to-point gof as described in 
+    """computes point-to-point gof as described in
     https://arxiv.org/abs/hep-ex/0203010.
+
     Input:
     - nevents_data n-dim data samples with shape (nevents_data, n)
     - nevents_reference n-dim referencesamples with shape (nevents_ref, n)
+
     Output:
-    Test Statistic based on 'Statisticsl Energy' """
+    Test Statistic based on 'Statisticsl Energy'
+
+    Samples must be pre-processed to have similar scale in each analysis dimension."""
 
     def __init__(self, data, reference_sample):
-        test_statistics_sample.__init__(self=self,
-                                        data=data,
-                                        reference_sample=reference_sample)
+        test_statistics_sample.__init__(
+            self=self, data=data, reference_sample=reference_sample
+        )
         self._name = self.__class__.__name__
         self.nevents_data = np.shape(self.data)[0]
         self.nevents_ref = np.shape(self.reference_sample)[0]
 
     def get_distances(self):
-        """get distances of data-data, reference-reference 
+        """get distances of data-data, reference-reference
         and data-reference"""
-        # TODO: What about scaling of different units/orders of magnitude?
-        # Calculate distance matrices for tthe two samples
-        dist = DistanceMetric.get_metric('euclidean')
+
+        dist = DistanceMetric.get_metric("euclidean")
 
         d_data_data = np.triu(dist.pairwise(self.data))
         d_data_data.reshape(-1)
@@ -100,12 +103,33 @@ class point_to_point_gof(test_statistics_sample):
         self.d_data_ref = dist.pairwise(
             self.data, self.reference_sample).reshape(-1)
 
-    def calculate_gof(self):
+    def get_d_min(self):
+        """find d_min as the average distance of reference_simulation
+        points in the region of highest density"""
+        # For now a very simple approach is chosen as the paper states that
+        # the precise value of this is not critical. One might want to
+        # look into a more proficient way in the future.
+        self.d_min = np.quantile(self.d_ref_ref, 0.001)
+
+    def weighting_function(self, d):
+        """Weigh distances d according to log profile. Pole at d = 0
+        is omitted by introducing d_min that replaces the distance for
+        d < d_min
+        """
+        if not hasattr(self, "d_min"):
+            self.get_d_min()
+        d[d <= self.d_min] = self.d_min
+
+        return -np.log(d)
+
+    def calculate_gof(self, *args, **kwargs):
+
         self.get_distances()
-        ret_data_data = 1/self.nevents_data**2 * \
-            np.sum(-np.log(self.d_data_data))
-        ret_ref_ref = 1/self.nevents_ref**2 * np.sum(-np.log(self.d_ref_ref))
-        ret_data_ref = -1/self.nevents_ref/self.nevents_data * \
-            np.sum(-np.log(self.d_data_ref))
+        ret_data_data = (1 / self.nevents_data ** 2 *
+                         np.sum(self.weighting_function(self.d_data_data)))
+        ret_ref_ref = (1 / self.nevents_ref ** 2 *
+                       np.sum(self.weighting_function(self.d_ref_ref)))
+        ret_data_ref = (-1 / self.nevents_ref / self.nevents_data *
+                        np.sum(self.weighting_function(self.d_data_ref)))
         ret = ret_data_data + ret_ref_ref + ret_data_ref
         return ret
