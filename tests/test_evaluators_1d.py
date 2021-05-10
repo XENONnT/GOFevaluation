cda9a5be7d13bcb5a3baf4ac36b6b7b3cf78f860
Robert Hammann <robert.hammann@outlook.com>
2021-05-10 16:39:15 +0200

    test for ks gof

--- tests/test_evaluators_1d.py
+++ tests/test_evaluators_1d.py
@@ -1,29 +1,41 @@
-# import GOFevaluation
-# import scipy.stats as sps
-# import numpy as np
+import scipy.stats as sps
+import numpy as np
+from scipy.interpolate import interp1d
+import unittest
+
+from GOFevaluation import kstest_gof
+# from GOFevaluation import kstest_two_sample_gof
 
 
-# bin_edges = np.linspace(-4, 4, 101)
-# bin_centers = bin_edges[:-1] + (bin_edges[1:] - bin_edges[:-1]) / 2
-# bin_widths = bin_edges[1:] - bin_edges[:-1]
-# normed_gauss_pdf = sps.norm.pdf(bin_centers) * bin_widths
+class Test_kstest_gof(unittest.TestCase):
+    def test_value(self):
+        """compare result of method to manually calculated gof"""
 
-# n_events = 1000
+        # Generate Test Data
+        n_samples = 100
+        # pseudo random data with fixed seed for reproducibility
+        data = sps.norm.rvs(size=n_samples, random_state=300)
 
-# expected_events = n_events * normed_gauss_pdf
-# data_points = sps.norm.rvs(size=1000)
+        bin_edges = np.linspace(-4, 4, 101)
+        bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
+        bin_widths = bin_edges[1:] - bin_edges[:-1]
+
+        normed_gauss_pdf = sps.norm.pdf(bin_centers) * bin_widths
+        interp_cdf = interp1d(bin_centers, np.cumsum(normed_gauss_pdf),
+                              kind='cubic')
+
+        # Calculate GoF 'by hand'
+        ecdf = np.arange(n_samples+1, dtype=float)/n_samples
+        dn = np.abs(interp_cdf(np.sort(data)) - ecdf[:-1])
+
+        # Calculate GoF
+        gofclass = kstest_gof(data=data,
+                              pdf=normed_gauss_pdf,
+                              bin_edges=bin_edges)
+        gof = gofclass.calculate_gof()
+
+        self.assertEqual(max(dn), gof)
 
 
-# from GOFevaluation import binned_poisson_gof
-# from GOFevaluation import chi2_gof
-
-
-# evaluator = GOFevaluation.evaluators_1d(data=data_points,
-#        pdf=normed_gauss_pdf,
-#        nevents_expected=n_events,
-#        bin_edges=bin_edges)
-# results = evaluator.calculate_gof_values()
-# print(results)
-
-# test = GOFevaluation.kstest_gof(data=data_points, pdf=normed_gauss_pdf, nevents_expected=n_events, bin_edges=bin_edges)
-# test.calculate_gof()
+if __name__ == "__main__":
+    unittest.main()
