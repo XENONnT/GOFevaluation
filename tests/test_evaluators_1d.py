5e400ddff9b65360a51a483a22c425627bdda819
Robert Hammann <robert.hammann@outlook.com>
2021-05-10 17:33:52 +0200

    unittest for two sample kstest

--- tests/test_evaluators_1d.py
+++ tests/test_evaluators_1d.py
@@ -4,7 +4,7 @@ from scipy.interpolate import interp1d
 import unittest
 
 from GOFevaluation import kstest_gof
-# from GOFevaluation import kstest_two_sample_gof
+from GOFevaluation import kstest_two_sample_gof
 
 
 class Test_kstest_gof(unittest.TestCase):
@@ -35,6 +35,40 @@ class Test_kstest_gof(unittest.TestCase):
         gof = gofclass.calculate_gof()
 
         self.assertEqual(max(dn), gof)
+
+
+class Test_kstest_two_sample_gof(unittest.TestCase):
+    def get_ecdf(self, values, data):
+        """Calculate ecdf by hand"""
+        n_data = len(data)
+        cdf = []
+        for value in values:
+            cdf.append(len(data[data <= value]))
+        cdf = np.array(cdf) / n_data
+        return cdf
+
+    def test_value(self):
+        """compare result of method to manually calculated gof"""
+
+        # Generate Test Data (simple case of n_sample=n_reference)
+        n_samples = 100
+        n_reference = 300
+        # pseudo random data with fixed seed for reproducibility
+        data = sps.norm.rvs(size=n_samples, random_state=300)
+        reference = sps.norm.rvs(size=n_reference, random_state=500)
+
+        # Calculate GoF 'by hand'
+        x = np.linspace(-3, 3, 10000)
+        ecdf_data = self.get_ecdf(x, data)
+        ecdf_reference = self.get_ecdf(x, reference)
+        dn = np.abs(ecdf_data - ecdf_reference)
+
+        # Calculate GoF
+        gofclass = kstest_two_sample_gof(
+            data=data, reference_sample=reference)
+        gof = gofclass.calculate_gof()
+
+        self.assertAlmostEqual(max(dn), gof, places=6)
 
 
 if __name__ == "__main__":
