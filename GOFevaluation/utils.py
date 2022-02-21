c0eb6fefcf2388d197f35760cb5810807145fd51
Robert Hammann <robert.hammann@outlook.com>
2022-02-21 12:35:41 -0600

    simpler test for finite bin edges

--- GOFevaluation/utils.py
+++ GOFevaluation/utils.py
@@ -357,7 +357,7 @@ def plot_equiprobable_histogram(data_sample, bin_edges, order=None,
             label = (r'$\sigma$-deviation from $\mu_\mathrm{{bin}}$ ='
                      + f'{midpoint:.1f} counts')
     elif(plot_mode == 'count_density'):
-        label = r'Counts per area expected in each bin'
+        label = r'Counts per area in each bin'
         ns = _get_count_density(ns, be_first, be_second, data_sample)
         cmap_str = kwargs.pop('cmap', 'viridis')
         cmap = mpl.cm.get_cmap(cmap_str)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -83,12 +83,20 @@ class TestEqpb(unittest.TestCase):
     def test__get_count_density(self):
         '''Test if _get_count_density can correctly count the density
         of data points in 2D  for both orderings'''
-        n_data = 12
+        # Define data that is equally spaced on a 6x6 grid.
+        # Binning 36 data points with a 3x2 partitions you get a
+        # count density of 1 in each bin.
+        sqrt_n_data = 6
         n_partitions = [2, 3]
-        data_sample = np.linspace(0, 1, n_data)
-        data_sample = np.vstack([data_sample for i in range(2)]).T
-        reference_sample = np.linspace(0, 1, int(10 * n_data))
-        reference_sample = np.vstack([reference_sample for i in range(2)]).T
+
+        x = np.linspace(0, sqrt_n_data, sqrt_n_data)
+        xx, yy = np.meshgrid(x, x)
+        data_sample = np.vstack([xx.reshape(-1), yy.reshape(-1)]).T
+
+        x = np.linspace(0, sqrt_n_data, int(sqrt_n_data * 10))
+        xx, yy = np.meshgrid(x, x)
+        reference_sample = np.vstack([xx.reshape(-1), yy.reshape(-1)]).T
+
         edges = []
         for order in [[0, 1], [1, 0]]:
             n, bin_edges = equiprobable_histogram(data_sample=data_sample,
@@ -99,22 +107,11 @@ class TestEqpb(unittest.TestCase):
             count_density = _get_count_density(n.copy(), edges[0],
                                                edges[1], data_sample)
             self.assertEqual(np.shape(n), np.shape(count_density))
-            testarray0 = np.array([[24.203389830508474,
-                                    24.203389830508474,
-                                    5.974895397489539],
-                                   [5.97489539748954,
-                                    24.203389830508463,
-                                    24.203389830508478]])
-            testarray1 = np.array([[36.61538461538461,
-                                    7.175879396984923],
-                                   [11.999999999999998,
-                                    11.999999999999998],
-                                   [7.175879396984927,
-                                    36.615384615384606]])
-
             for i in range(0, len(edges[0]) - 1):
                 for j in range(0, len(edges[1][i]) - 1):
                     if(order == [0, 1]):
-                        self.assertEqual(testarray0[i][j], count_density[i][j])
+                        self.assertAlmostEqual(1, count_density[i][j],
+                                               places=12)
                     elif(order == [1, 0]):
-                        self.assertEqual(testarray1[i][j], count_density[i][j])
+                        self.assertAlmostEqual(1, count_density[i][j],
+                                               places=12)
